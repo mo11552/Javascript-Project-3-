034cd9fcc4aa1a11098ee0f075e197b8a27a493a
Moyo Olojede <Moyo@Moyos-MBP.home>
2021-06-27 21:37:18 -0400

    Code has been changed to include the answer the computer chose. Also the number now resets after the game is over and the var i has been changed to numberOfTries.

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ reveal the number the computer chose to them, and let them know if they got it r
 */
 var x = Math.floor(Math.random() * 100 + 1);
 var maxTries = 3;
-var i = 0
+var numberOfTries = 0
 document.getElementById("submitguess").onclick = function() {
   var y = document.getElementById("guessField").value;
-  i += 1;
-  if(i > maxTries)
+  numberOfTries += 1;
+  if(numberOfTries >= maxTries)
   {
-    alert("You have no more tries");
+    alert("You have no more tries" + " " + "The answer is" + " " + x);
+    window.location.reload(false);
   }
   
   if(x == y)
